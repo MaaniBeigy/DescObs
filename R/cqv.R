d285733534a11259fbd6d00918a2a74fb6a9c0a0
Maani Beigy <m-beigy@student.tums.ac.ir>
2019-02-04 13:53:07 +0330

    documentation and example added for cqv function

---
+++ R/cqv.R
@@ -0,0 +1,44 @@
+#' Coefficient of Quartile Variation (CQV)
+#'
+#' Generic function for the coefficient of quartile variation (cqv)
+#'
+#' @param x An {R} object. Currently there are methods for numeric vectors
+#' @param na.rm a logical value indicating whether \code{NA} values should be stripped before the computation proceeds.
+#' @digits integer indicating the number of decimal places to be used.
+#' @example ./examples/cqv.R
+#' @references Bonett, DG., 2006, Confidence interval for a coefficient of quartile variation, Computational Statistics & Data Analysis, 50(11), 2953-7, \href{https://doi.org/10.1016/j.csda.2005.05.007}{DOI}
+#' @export
+cqv <- function(x, na.rm, digits) {
+    if (!is.numeric(x)) {
+        warning("argument is not numeric: returning NA")
+        return(NA_real_)
+    }
+    na.rm = na.rm  # removes NAs if TRUE
+    digits = digits  # digits required for rounding
+    q3 <- unname(
+        quantile(
+            x,
+            probs = 0.75,  # third quartile (0.75 percentile)
+            na.rm = na.rm
+        )
+    )
+    q1 <- unname(
+        quantile(
+            x,
+            probs = 0.25,  # first quartile (0.75 percentile)
+            na.rm = na.rm
+        )
+    )
+    if (q3 == 0) {  # to avoid NaNs when q3 and q1 are zero
+        q3 <- max(x)
+    }
+    cqv <- 100*(
+        (q3 - q1)/(q3 + q1)
+    )  # coefficient of quartile variation (CQV)
+    return(
+        round(  # round output
+            cqv,
+            digits = digits
+        )
+    )
+}
---
+++ examples/cqv.R
@@ -0,0 +1,2 @@
+x <- rnorm(100, mean = 10, sd = 6)
+cqv(x, na.rm = TRUE, digits = 2)
