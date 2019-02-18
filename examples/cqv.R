631ad3768f5651b807db7492c1df24075946bc53
MaaniBeigy <manibeygi@gmail.com>
2019-02-19 02:49:08 +0330

    examples updated

--- examples/cqv.R
+++ examples/cqv.R
@@ -3,26 +3,11 @@ x <- c(
     4.6, 5.4, 5.4, 5.7, 5.8, 5.9, 6.0, 6.6, 7.1, 7.9
 )
 cqv(x)
-cqv(x, na.rm = TRUE)
-# cqv
-# 45.625
 cqv(x, na.rm = TRUE, digits = 2)
-# cqv
-# 45.62
 cqv(x, na.rm = TRUE, digits = 2, CI = "Bonett")
-# cqv     Bonett's 95% CI
-#   "45.62" "24.78-77.33"
 cqv(x, na.rm = TRUE, digits = 2, CI = "norm")
-# cqv     normal approximation 95% CI
-# "45.62" "19.95-71.04"
 cqv(x, na.rm = TRUE, digits = 2, CI = "basic")
-# cqv     basic bootstrap 95% CI
-# "45.62" "19.5-73.37"
 cqv(x, na.rm = TRUE, digits = 2, CI = "perc")
-# cqv     bootstrap percentile 95% CI
-# "45.62" "16.22-68.68"
 cqv(x, na.rm = TRUE, digits = 2, CI = "bca")
-# cqv     adjusted bootstrap percentile (BCa) 95% CI
-# "41.74" "23.69-80.39"
-
+cqv(x, na.rm = TRUE, digits = 2, CI = "all")
 
