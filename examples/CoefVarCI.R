9832b306ac2d35942154e29a196966b1671a113d
MaaniBeigy <manibeygi@gmail.com>
2019-03-18 16:15:01 +0330

    minor edit

--- examples/CoefVarCI.R
+++ examples/CoefVarCI.R
@@ -2,9 +2,9 @@ y <- c(
     0.2, 0.5, 1.1, 1.4, 1.8, 2.3, 2.5, 2.7, 3.5, 4.4,
     4.6, 5.4, 5.4, 5.7, 5.8, 5.9, 6.0, 6.6, 7.1, 7.9
 )
-CoefVarCI$new(y)$kelley_ci()
+CoefVarCI$new(x = y)$kelley_ci()
 cv_y <- CoefVarCI$new(
-    y,
+    x = y,
     alpha = 0.05,
     R = 1000,
     digits = 2,
